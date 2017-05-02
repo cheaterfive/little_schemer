b2d4f5a358699d0417f06d6415f768fa116d29b0
Richard Smart <thequanglewangle@gmail.com>
2017-05-02 08:05:41 +0100

    insertl

---
+++ insertl.scm
@@ -0,0 +1,31 @@
+; Build a list from lat with new before old
+(define insertl
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons new
+	      (cons old (insertl new
+				 old
+				 (cdr lat)))))
+       (else (cons (car lat)
+		   (insertl new
+			    old
+			    (cdr lat))))
+       )
+      )
+     )
+    )
+  )
+
+
+(define (main args)
+  (define input_lat '(ice cream with fudge for dessert))
+  (define new 'topping)
+  (define old 'fudge)
+  (define result '(ice cream with topping fudge for dessert))
+  (print (insertl new old input_lat))
+  (print (equal? result  (insertl new old input_lat)))
+)
