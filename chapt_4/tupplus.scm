8c2156cede30dfd43d384e0a6fb4c1c7257c07d5
Richard Smart <thequanglewangle@gmail.com>
2017-05-05 13:13:44 +0100

    make tupplus variable length

--- chapt_4/tupplus.scm
+++ chapt_4/tupplus.scm
@@ -4,7 +4,8 @@
 (define tup+
   (lambda (tup1 tup2)
     (cond
-     ((and (null? tup1) (null? tup2)) (quote ()))
+     ((null? tup1) tup2)
+     ((null? tup2) tup1)
      (else (cons (plus (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2))))
      )
     )
@@ -14,6 +15,7 @@
   (print (equal? '(9) (tup+ '(4) '(5))))
   (print (equal? '(4 4) (tup+ '(0 4) '(4 0))))
   (print (equal? '(4 4) (tup+ '(2 3) '(2 1))))
+  (print (equal? '(4 4 7) (tup+ '(2 3 7) '(2 1))))
   (print (equal? '() (tup+ '() '())))  
   )
 	   
