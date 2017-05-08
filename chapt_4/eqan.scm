a0395c9077adf1e8b9cbbc4cb619878767544399
Richard Smart <thequanglewangle@gmail.com>
2017-05-08 08:44:45 +0100

    eqan

--- chapt_4/eqan.scm
+++ chapt_4/eqan.scm
@@ -4,7 +4,7 @@
   (lambda (n1 n2)
     (cond
      ((and (number? n1) (number? n2)) (= n1 n2))
-     ((or (number? n1) (number> n2)) #f)
+     ((or (number? n1) (number? n2)) #f)
      (else (eq? n1 n2))
      )
     )
@@ -13,6 +13,6 @@
 (define (main args)
   (print (equal? #t (eqan 3 3)))
   (print (equal? #f (eqan 4 3)))
-  (print (equal? #f (eqan a 3)))
-  (print (equal? #t (eqan a a)))
+  (print (equal? #f (eqan 'a 3)))
+  (print (equal? #t (eqan 'a 'a)))
   )
