62c60e6171b29f5a1c93a648e86046ba7c9c600c
Richard Smart <thequanglewangle@gmail.com>
2017-05-08 08:22:59 +0100

    eqan

---
+++ chapt_4/eqan.scm
@@ -0,0 +1,15 @@
+(load "my_eq.scm")
+
+(define eqan
+  (lambda (n1 n2)
+    (cond
+     ((and (number? n1) (number? n2)) (= n1 n2))
+     ((or (number? n1) (number> n2)) #f)
+     (else (eq? n1 n2))
+     )
+    )
+  )
+
+(define (main args)
+  (print (equal? #t (eqan 3 3)))
+  )
