c622cfa4e5ef11e24e2bff9f82d89639fc88915e
Richard Smart <thequanglewangle@gmail.com>
2017-05-08 08:06:52 +0100

    all nums

---
+++ chapt_4/all-nums.scm
@@ -0,0 +1,20 @@
+(load "length.scm")
+(load "sub1")
+
+(define all-nums
+  (lambda (lat)
+    (cond
+     ((null? lat) '())
+     ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+     (else (all-nums (cdr lat)))
+     )
+    )
+  )
+
+(define (main args)
+  ;(print (all-nums '(6 3 hotdog)))
+  (print (equal? '(6 3) (all-nums '(6 3 hotdog))))
+  (print (equal? '(6 3) (all-nums '(6 top 3 hotdog))))
+  (print (equal? '(6) (all-nums '(6))))
+  (print (equal? '() (all-nums '())))
+  )
